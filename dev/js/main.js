322f100ac422aca1b8e6fdbbd873b338f3b5a622
Tanya Kovalenko <tanya.griz89@gmail.com>
2019-12-16 20:32:16 +0200

    add main.js

---
+++ dev/js/main.js
@@ -0,0 +1,63 @@
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 3,
+    mobileFirst: true,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 3,
+                infinite: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 3,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 3,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                dots: true
+            }
+        }
+    ]
+});
+
+$('.slider-populer').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 3,
+    mobileFirst: true,
+    arrows: false,
+    dots: true,
+    responsive: [
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true
+            }
+        }
+    ]
+});
+
+
+$('.single-item').slick();
