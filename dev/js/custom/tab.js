09706964a0873e98afb7c7414801f87495f54cc7
Tanya Kovalenko <tanya.griz89@gmail.com>
2020-09-11 10:27:30 +0300

    added timer

--- dev/js/custom/tab.js
+++
@@ -1,8 +0,0 @@
-$('#myTab a').on('click', function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-})
-
-$(window).on("scroll", function() {
-    $('#fixed-bar').toggleClass("bg-color", window.scrollY > 92);
-});
